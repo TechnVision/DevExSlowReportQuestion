40630cbbbdd1b94e7e7a19fa0fe9bcc583e3f3eb
Sohil Ahmed <sohil.ahmed07@outlook.com>
2024-03-27 00:50:03 +0530

    added gitignore

--- DevExSlowReportQuestion/obj/Debug/net8.0-windows/DevExSlowReportQuestion.AssemblyInfo.cs
+++ DevExSlowReportQuestion/obj/Debug/net8.0-windows/DevExSlowReportQuestion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevExSlowReportQuestion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+481ccf0a625cc4677f854b40b89365354f7e9aeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevExSlowReportQuestion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevExSlowReportQuestion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
